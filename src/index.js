a7d0a7d93ae0359b75689d826e05eb88108249c9
Linda Angulo Lopez <angulo.linda@gmail.com>
2021-08-31 15:20:35 +0200

    exo 1-4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 // On importe la librairie fastify
 const fastify = require('fastify')
 
@@ -7,14 +8,25 @@ const fastify = require('fastify')
 const app = fastify({ logger: true })
 
 // Création d'une route GET sur le chemin "/"
-app.get('/', function () {
+app.get('/', (request) => {
   return 'Coucou les amis !!!'
 })
 
+const a = () => {
+  return { status: 200 }
+}
+
+// Récupére les catégories
+app.get('/categories', () => ['animale', 'nature', 'science', 'technologie'])
+
+// Création d'une catégorie
+app.post('/categories', () => ({ status: 200 }))
+
+// Récupére les articles
+app.get('/articles', () => [{ title: 'Mon premier article' }])
+
+// Création d'un article
+app.post('/articles', () => ({ status: 200 }))
+
 // On lance le serveur sur le port 8080
 app.listen(8080)
-
-
-app.get('/categories', function () {
-  return  `['animale', 'nature', 'science', 'technologie']`
-})
