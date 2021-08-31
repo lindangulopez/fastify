dacd20c44832d602abf095edaa7ed27a468ba707
Linda Angulo Lopez <angulo.linda@gmail.com>
2021-08-31 13:30:50 +0200

    fastify setr up env

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// On importe la librairie fastify
+const fastify = require('fastify')
+
+// Créer une application fastify.
+// Une application fastify c'est ce qui vas contenir
+// toutes nos routes
+const app = fastify({ logger: true })
+
+// Création d'une route GET sur le chemin "/"
+app.get('/', function () {
+  return 'Coucou les amis !!!'
+})
+
+// On lance le serveur sur le port 8080
+app.listen(8080)
+
+
+app.get('/categories', function () {
+  return  `['animale', 'nature', 'science', 'technologie']`
+})
